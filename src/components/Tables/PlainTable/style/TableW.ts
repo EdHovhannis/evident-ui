309a96f1a2a136acec6fe78d89016f1d521f54e4
Edgar <ed.hovhannis1992@gmail.com>
2024-03-09 20:17:35 +0300

    add table

--- src/components/Tables/PlainTable/style/TableW.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const TableW = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
--- src/components/Tables/PlainTable/style/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './TableW';
-export * from './TableColumns';
-export * from './TableRow';
-export * from './TableCol';
